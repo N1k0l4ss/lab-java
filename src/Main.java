52dc7b1ad3726fe8acba9a7cda988ea37236da49
N1k0l4ss <67698184+N1k0l4ss@users.noreply.github.com>
2020-10-18 20:57:48 +0300

    request228

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import static java.lang.Math.*;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Test");
+
+    public static void main(String[] args) {
+
     }
-} // test
+
+    public double calcY(int p, int i, double a, double h)
+    {
+        double x = a + i * h;
+        double res = 0;
+        if (x < 1.7)
+            res = (p*x*p*x) - 7 / (x*x);
+        else if (x >= 1.7 && x < 1.8)
+            res = pow(a*x, 3) + 7*sqrt(x);
+        else if (x>1.7)
+            res = log10(x+7*sqrt(x));
+        return res;
+    }
+
+}
--- test/MainTest.java
+++ test/MainTest.java
@@ -1,8 +1,23 @@
-//import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    Main main;
 
-//    @org.junit.jupiter.api.BeforeEach
+    @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        main = new Main();
+    }
+
+    @Test
+    void testCalcY()
+    {
+        int i = 240;
+        double h = 0.005;
+        int p = 4;
+        double a = 1.5;
+        double res = main.calcY(p, i, a, h);
+        assertEquals(a, res, h, "y must be a x*x");
     }
 }
