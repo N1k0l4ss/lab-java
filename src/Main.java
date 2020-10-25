76c8505882fcabca3dfdf97669673a2768dfc4f2
N1k0l4ss <67698184+N1k0l4ss@users.noreply.github.com>
2020-10-25 12:38:27 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,18 @@ public class Main {
     double a = 1.5;
     double p = 4;
 
+    public static void main(String[] args) {
+        Main mn = new Main();
+        double x1 = 0.8;
+        double x2 = 2;
+        double deltaX = 0.005;
+        mn.fillX(x1, x2, deltaX);
+        mn.fillY(x1, x2, deltaX);
+        System.out.println("Max y = " + mn.maxY());
+        System.out.println("Min y = " + mn.minY());
+        System.out.println("Averange y = " + mn.averangeY());
+    }
+
     public double calcY(double x)
     {
         double res = 0;
@@ -27,17 +39,46 @@ public class Main {
     public void fillY(double x1, double x2, double deltaX)
     {
         y = new double[findSize(x1,x2,deltaX)];
-        for (int i = 0; i < y.length; i++) {
+        for (int i = 0; i < y.length; i++)
             y[i] = calcY(x[i]);
-        }
     }
 
     public void fillX(double x1, double x2, double deltaX)
     {
         x = new double[findSize(x1,x2,deltaX)];
-        for (int i = 0; i < x.length; i++) {
+        for (int i = 0; i < x.length; i++)
             x[i] = x1 + deltaX * i;
-        }
+    }
+
+    public double maxY()
+    {
+        double max = y[0];
+        for (int i = 0; i < y.length; i++)
+            if (y[i]>max)
+                max = y[i];
+        return max;
+    }
+
+    public double minY()
+    {
+        double min = y[0];
+        for (int i = 0; i < y.length; i++)
+            if (y[i]<min)
+                min = y[i];
+        return min;
+    }
+
+    public double sumY()
+    {
+        double sum = 0;
+        for (int i = 0; i < y.length; i++)
+            sum += y[i];
+        return sum;
+    }
+
+    public double averangeY()
+    {
+        return sumY()/y.length;
     }
 }
 
--- test/MainTest.java
+++ test/MainTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     Main main;
+    double x1 = 0.8;
+    double x2 = 2;
+    double deltaX = 0.005;
 
     @BeforeEach
     void setUp() {
@@ -12,19 +15,10 @@ class MainTest {
     }
 
     @Test
-    void testCalcY()
-    {
-        double expected = 0.6975;
-        main.fillX(0.8, 2, 0.005);
-        double res = main.calcY(main.x[0]);
-        assertEquals(expected, res, 0.00001, "Wrong calcY");
-    }
-
-    @Test
     void testFindSize()
     {
         double expected = 241;
-        double res = main.findSize(0.8, 2, 0.005);
+        double res = main.findSize(x1, x2, deltaX);
         assertEquals(expected, res, "Wrong findSizeX");
     }
 
@@ -32,21 +26,67 @@ class MainTest {
     void testFillX()
     {
         double expected = 2;
-        main.fillX(0.8, 2, 0.005);
+        main.fillX(x1, x2, deltaX);
         double res = main.x[240];
         assertEquals(expected, res, 0.00001, "Wrong fillX");
     }
 
     @Test
+    void testCalcY()
+    {
+        double expected = 0.6975;
+        main.fillX(x1, x2, deltaX);
+        double res = main.calcY(main.x[0]);
+        assertEquals(expected, res, 0.00001, "Wrong calcY");
+    }
+
+    @Test
     void testFillY()
     {
-        double x1 = 0.8;
-        double x2 = 2;
-        double deltaX = 0.005;
         main.fillX(x1, x2, deltaX);
         main.fillY(x1, x2, deltaX);
         double expected = 0.6975;
         double res = main.y[0];
         assertEquals(expected, res, 0.00001, "Wrong fillY");
+    }
+
+    @Test
+    void testMinY()
+    {
+        main.fillX(x1, x2, deltaX);
+        main.fillY(x1, x2, deltaX);
+        double expected = 0.089;
+        double res = main.minY();
+        assertEquals(expected, res, 0.00001, "Wrong minY");
+    }
+
+    @Test
+    void testMaxY()
+    {
+        main.fillX(x1, x2, deltaX);
+        main.fillY(x1, x2, deltaX);
+        double expected = 43.53194;
+        double res = main.maxY();
+        assertEquals(expected, res, 0.00001, "Wrong maxY");
+    }
+
+    @Test
+    void testSumY()
+    {
+        main.fillX(x1, x2, deltaX);
+        main.fillY(x1, x2, deltaX);
+        double expected = 3837.33993;
+        double res = main.sumY();
+        assertEquals(expected, res, 0.00001, "Wrong sumY");
+    }
+
+    @Test
+    void testAverangeY()
+    {
+        main.fillX(x1, x2, deltaX);
+        main.fillY(x1, x2, deltaX);
+        double expected = 15.92257;
+        double res = main.averangeY();
+        assertEquals(expected, res, 0.00001, "Wrong averangeY");
     }
 }
