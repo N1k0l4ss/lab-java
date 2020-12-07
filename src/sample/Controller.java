507bada4b26a06474c0a40aafc2e8685f623070e
N1k0l4ss <67698184+N1k0l4ss@users.noreply.github.com>
2020-12-07 10:37:35 +0200

    Done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,9 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import logic.Lab2;
 
 public class Controller {
